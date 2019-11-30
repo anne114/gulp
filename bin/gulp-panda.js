b2d7bfb3ae281e60bb7cc1e36251d5bd547ba387
anne114 <252848509@qq.com>
2019-11-30 17:41:48 +0800

    gulp模块封装

---
+++ bin/gulp-panda.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+process.argv.push('--cwd');
+process.argv.push(process.cwd());
+process.argv.push("--gulpfile");
+process.argv.push(require.resolve("../lib/index.js"))
+require('gulp/bin/gulp'); //执行node_modules/gulp/bin/gulp.js文件
+console.log('aa')
